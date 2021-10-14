0e2d23c3d31a1ead7fc232401e6b273fc306e7a9
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-14 12:30:58 +0530

    Create solution_c4_saa2.py

---
+++ solution_c4_saa2.py
@@ -0,0 +1,13 @@
+# Defining a function 'square()' with 'side' as parameter
+def square(side):
+    # Calculate 'area' 
+    area = side*side
+    # Calculate 'volume'
+    volume = side*side*side
+    # Printing the results
+    print("Area of the square:",area)
+    print("Volume of the square:",volume)
+
+  
+# Calling the function 'square()' with '8' as argument
+square(8)
